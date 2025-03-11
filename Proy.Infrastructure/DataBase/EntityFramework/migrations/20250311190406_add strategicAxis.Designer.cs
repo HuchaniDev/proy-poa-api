eaa3ea599d68c0f5850886f92c8960b4f57b8323
Daniel Huchani <161368230+HuchaniDev@users.noreply.github.com>
2025-03-11 16:16:17 -0400

    feat: change token expire

--- Proy.Infrastructure/DataBase/EntityFramework/migrations/20250311190406_add strategicAxis.Designer.cs
+++ Proy.Infrastructure/DataBase/EntityFramework/migrations/20250311190406_add strategicAxis.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Proy.Infrastructure.DataBase.EntityFramework.Context;
 
@@ -11,9 +12,11 @@ using Proy.Infrastructure.DataBase.EntityFramework.Context;
 namespace Proy.Infrastructure.Database.EntityFramework.migrations
 {
     [DbContext(typeof(ProyDbContext))]
-    partial class ProyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250311190406_add strategicAxis")]
+    partial class addstrategicAxis
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Proy.Infrastructure/DataBase/EntityFramework/migrations/20250311190406_add strategicAxis.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proy.Infrastructure.Database.EntityFramework.migrations
+{
+    /// <inheritdoc />
+    public partial class addstrategicAxis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StrategicAxis",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Code = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: false),
+                    LastModifiedByAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    LastModifiedBy = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StrategicAxis", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "StrategicLines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    StrategicAxisId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: false),
+                    LastModifiedByAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    LastModifiedBy = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StrategicLines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StrategicLines_StrategicAxis_StrategicAxisId",
+                        column: x => x.StrategicAxisId,
+                        principalTable: "StrategicAxis",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "StrategicActions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    StrategicLineId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: false),
+                    LastModifiedByAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    LastModifiedBy = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StrategicActions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StrategicActions_StrategicLines_StrategicLineId",
+                        column: x => x.StrategicLineId,
+                        principalTable: "StrategicLines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StrategicActions_StrategicLineId",
+                table: "StrategicActions",
+                column: "StrategicLineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StrategicLines_StrategicAxisId",
+                table: "StrategicLines",
+                column: "StrategicAxisId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StrategicActions");
+
+            migrationBuilder.DropTable(
+                name: "StrategicLines");
+
+            migrationBuilder.DropTable(
+                name: "StrategicAxis");
+        }
+    }
+}
